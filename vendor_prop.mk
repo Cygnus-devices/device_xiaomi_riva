4491c2f1e812782092fc1e464667c61ff55b26ad
Sultan Alsawaf <sultan@kerneltoast.com>
2020-05-02 23:27:59 +0200

    riva: Force triple frame buffers
    This yields significantly reduced jitter and doesn't have any measurable
    impact on latency.
    
    * also sort surfaceflinger props
    
    Change-Id: Ie038b54f21717ca656c3278dfd408df48f8da152

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -172,6 +172,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
      persist.vendor.data.iwlan.enable=true \
      ro.telephony.iwlan_operation_mode=legacy
 
+# Surfaceflinger
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.surface_flinger.max_virtual_display_dimension=4096 \
+    ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=3
+
 # Time Services
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.delta_time.enable=true \
