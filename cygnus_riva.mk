ea4ed9d12deed3ed0186295fe99e651175e48ad6
Uptownboys32 <anggaalfian133@gmail.com>
2020-05-08 02:06:27 +0000

    riva: Cygnus-ify

--- cygnus_riva.mk
+++ cygnus_riva.mk
@@ -20,8 +20,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
-# Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Cygnus stuff
+$(call inherit-product, vendor/cygnus/configs/common.mk)
+include device/qcom/common/common.mk
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Inherit from riva device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -30,7 +36,7 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := riva
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Redmi 5A
-PRODUCT_NAME := lineage_riva
+PRODUCT_NAME := cygnus_riva
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
