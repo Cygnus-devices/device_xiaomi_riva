9e151c6106a1110adbe76c13622f26c4133064bf
Uptownboys32 <anggaalfian133@gmail.com>
2020-05-24 23:49:41 +0000

    riva: prebuilts: Add some Oneplus apps

--- device.mk
+++ device.mk
@@ -265,6 +265,14 @@ PRODUCT_PACKAGES += \
     libOmxVenc \
     libstagefrighthw
 
+# OPCalculator
+PRODUCT_PACKAGES += \
+    OPCalculator
+
+# OPMessages
+PRODUCT_PACKAGES += \
+    OPMessages
+
 # Permissions
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml \
