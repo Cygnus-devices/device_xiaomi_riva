417f6bc0c04be3d6ed5707077171a090d330b9fd
adi153 <singhmanohar6699@gmail.com>
2020-07-08 03:52:16 +0000

    riva: Add props to improve battery backup

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -141,6 +141,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.extension_library=libqti-perfd-client.so \
 
+# Power Saver
+PRODUCT_PROPERTY_OVERRIDES += \
+    power.saving.mode=1 \
+    pm.sleep_mode=1
+
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
     rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
@@ -151,7 +156,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.radio.custom_ecc=1 \
     persist.vendor.radio.rat_on=combine \
     persist.vendor.radio.sib16_support=1 \
-    persist.vendor.data.mode=concurrent
+    persist.vendor.data.mode=concurrent \
+    ro.vold.umsdirtyratio=20 \
+    ro.ril.disable.power.collapse=0
 
 # Time Services
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -170,7 +177,8 @@ ro.debuggable=1
 
 # Wifi
 PRODUCT_PROPERTY_OVERRIDES += \
-    wifi.interface=wlan0
+    wifi.interface=wlan0 \
+    wifi.supplicant_scan_interval=500
 
 # Memory management tweaks
 ro.config.fha_enable=true
