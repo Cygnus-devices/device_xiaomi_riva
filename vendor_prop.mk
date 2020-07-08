17a5e300269f6d01777e0b734ab2151cfac4c728
Uptownboys32 <anggaalfian133@gmail.com>
2020-07-08 23:28:02 +0000

    riva: Enable USB debugging
    and remove some stuff

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -162,6 +162,13 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.usb.config.extra=none
 
+# USB debugging
+PRODUCT_PROPERTY_OVERRIDES += \
+persist.sys.usb.config=mtp,adb \
+ro.adb.secure=0 \
+ro.secure=0 \
+ro.debuggable=1
+
 # Wifi
 PRODUCT_PROPERTY_OVERRIDES += \
     wifi.interface=wlan0
