24ba7e4ae73a9be91f7e5b017feb9e4eea0f8074
Uptownboys32 <anggaalfian133@gmail.com>
2020-07-14 02:14:43 +0000

    riva: prop: Improve touch

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -164,6 +164,25 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.delta_time.enable=true \
 
+# Touch
+PRODUCT_PROPERTY_OVERRIDES += \
+	touch.deviceType=touchScreen \
+	touch.orientationAware=1 \
+	touch.size.calibration=diameter \
+	touch.size.scale=1 touch.size.bias=0 \
+	touch.size.isSummed=0 \
+	touch.pressure.calibration=amplitude \
+	touch.pressure.scale=0.001 \
+	touch.orientation.calibration=none \
+	touch.distance.calibration=none \
+	touch.distance.scale=0 \
+	touch.coverage.calibration=box \
+	touch.gestureMode=spots \
+	MultitouchSettleInterval=1ms \
+	MultitouchMinDistance=1px \
+	TapInterval=1ms \
+	TapSlop=1p
+
 # Usb
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.usb.config.extra=none
